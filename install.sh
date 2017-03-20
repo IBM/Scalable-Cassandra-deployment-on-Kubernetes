4e5fe23ab021aa9e38e82afddd39f9dd98316c9a
Ishan Gulhane <ishan@Ishans-MacBook-Pro.local>
2017-03-20 11:12:09 -0700

    Test .travis.yml file

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+
 wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
 echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
 sudo apt-get update

0c917373c38e29545940c1053dc49997e220c4d5
Ishan Gulhane <ishan@Ishans-MacBook-Pro.local>
2017-03-20 11:15:14 -0700

    Test .travis.yml file

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
-wget -q -O - https://packages.cloudfoundry.org/debian/cli.cloudfoundry.org.key | sudo apt-key add -
-echo "deb http://packages.cloudfoundry.org/debian stable main" | sudo tee /etc/apt/sources.list.d/cloudfoundry-cli.list
-sudo apt-get update
-sudo apt-get install cf-cli
+curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
+mv cf /usr/local/bin
+curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
 cf --version
 tar -xvf Bluemix_CLI.tar.gz
 cd Bluemix_CLI
-sudo ./install_bluemix_cli
+./install_bluemix_cli

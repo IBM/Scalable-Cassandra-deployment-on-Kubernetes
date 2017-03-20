2a1e74d9a72a7de5ae59c88e1c82c91c235835e0
Ishan Gulhane <ishan@Ishans-MacBook-Pro.local>
2017-03-20 11:18:31 -0700

    Test .travis.yml file

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 curl -L "https://cli.run.pivotal.io/stable?release=linux64-binary&source=github" | tar -zx
 mv cf /usr/local/bin
-curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
+sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
 cf --version
 tar -xvf Bluemix_CLI.tar.gz
 cd Bluemix_CLI

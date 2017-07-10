bb5ebdb56d37a462d15639bf6aa8d1b045d35125
Anthony Amanse <joe.amanse@obf.ateneo.edu>
2017-07-10 11:57:18 -0700

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -23,8 +23,8 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 }
 
 function cluster_setup() {
-bx cs workers anthony-cluster-travis
-$(bx cs cluster-config anthony-cluster-travis | grep export)
+bx cs workers $CLUSTER_NAME
+$(bx cs cluster-config $CLUSTER_NAME | grep export)
 kubectl delete --ignore-not-found=true -f cassandra-service.yaml
 kubectl delete --ignore-not-found=true -f cassandra-controller.yaml
 kubectl delete --ignore-not-found=true -f cassandra-statefulset.yaml

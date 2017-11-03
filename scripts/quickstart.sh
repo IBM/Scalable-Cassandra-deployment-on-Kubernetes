6533acb06ef8a0ec951e7a5171cc10eeeb75652a
Paul Czarkowski <username.taken@gmail.com>
2017-11-03 10:21:25 -0500

    fix tests to use kube in travis
    see: https://blog.travis-ci.com/2017-10-26-running-kubernetes-on-travis-ci-with-minikube

--- scripts/quickstart.sh
+++ scripts/quickstart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 kubectl create -f cassandra-service.yaml
-kubectl create -f cassandra-controller.yaml
+kubectl create -f local-volumes.yaml
+kubectl create -f cassandra-statefulset.yaml
 kubectl get nodes
 kubectl get svc cassandra
